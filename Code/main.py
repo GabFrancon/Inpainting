e1ae3ed1f793d5e4981be26c02e2320bd91075d7
Gabriel FRANCON <gab.francon@gmail.com>
2021-10-01 15:43:15 +0200

    added default arguments for cmd parser

--- Code/main.py
+++ Code/main.py
@@ -1,39 +1,47 @@
+import os
 import argparse as ap
 import imageio as imo
 from Inpainter import Inpainter
 
 
 def main():
+    """ Usage in inpainting directory :
+
+        python Code/main.py -i Data/Image.png -m Data/Mask.png -o Data/Output.png """
+
     args = parse_arguments()
     image = imo.imread(args.input)
     mask = imo.imread(args.mask)
     output_filepath = args.output
 
     output_image = Inpainter(image, mask).inpaint()
-    imo.imwrite(output_filepath, output_image[:, :, 0])
+    imo.imwrite(output_filepath, output_image)
+    os.system('imagej '+output_filepath+' &')
+
+    print('inpainting process finished')
 
 
 def parse_arguments():
     parser = ap.ArgumentParser()
-    parser.add_argument\
-    (
+    parser.add_argument(
         '-i',
         '--input',
-        help='the filepath to the image containing object to be edited'
+        help='the filepath to the image containing object to be edited',
+        default='../Data/Image.png'
     )
 
-    parser.add_argument\
-    (
+    parser.add_argument(
         '-m',
         '--mask',
-        help='the mask of the region to be removed'
+        help='the mask of the region to be removed',
+        default='../Data/Mask.png'
     )
 
-    parser.add_argument\
-    (
+    parser.add_argument(
         '-o',
         '--output',
-        help='the filepath to save the output image'
+        help='the filepath to save the output image',
+        default='../Data/Output.png'
     )
 
     return parser.parse_args()
