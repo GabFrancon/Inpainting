5c0d9b7fb90d12f6d9d05afb5cf3039671eeb201
Anselme DONATO <anselme.donato@telecom-paris.fr>
2021-10-05 18:26:58 +0200

    Première implémentation de is_finished

--- Code/Inpainter.py
+++ Code/Inpainter.py
@@ -3,6 +3,9 @@ import numpy as np
 
 class Inpainter:
     def __init__(self, image, mask):
+        self safetyCount = 0
+        self safetyMax = self.image.shape[0] * self.image.shape [1] 
+        #C'est juste une séurité pour que l'algorithme se termine: on calcule le nombre de pixels de l'image, et si on fait plus de boucles qu'il n'y a de pixels dans l'image c'est qu'il y a un problème. Cf isFinished
         self.image = np.copy(image.astype('uint8'))
         self.mask = np.copy(mask.round().astype('uint8'))
         self.confidence = None
@@ -57,8 +60,11 @@ class Inpainter:
     def update_confidence(self):
         print('update confidence\n')
 
-    def is_finished(self):
+    def is_finished(self): 
         print('test if process finished\n')
-        return True
-
+        safetyCount += 1 #On pourrait faire += 9 je crois mais dans le doute ... c'est quand même une sécurité 
+        if (size(confidence) == 0) or (safetyCount < safetyMax): #Enfin ouais mais ça ne marche que si confidence ne contient que les pixels dont on n'est pas sûr 
+            return True
+        else: 
+            return False 
 
